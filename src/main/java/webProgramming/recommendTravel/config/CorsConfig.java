277f0d9e9c0601f80c413b8801680492e8c533e4
wkdal1433 <126257712+wkdal1433@users.noreply.github.com>
2023-12-19 04:41:26 +0900

    merge
    merge용

--- src/main/java/webProgramming/recommendTravel/config/CorsConfig.java
+++ src/main/java/webProgramming/recommendTravel/config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+    @Override //머지용
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
                 .allowedOrigins("http://localhost:3000") // 프론트엔드 서버의 정확한 URL
